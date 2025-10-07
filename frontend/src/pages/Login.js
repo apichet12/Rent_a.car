f48ff8716f46f5e1c5b4d2d35d93415b5606d3a7
apichet12 <apichetphiromrak424@gmail.com>
2025-10-07 22:16:57 +0700

    แก้

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -47,11 +47,12 @@ const Login = () => {
       return;
     }
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://rentacar-0kj9.onrender.com/api/login", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ username: form.username, password: form.password }),
 });
+
 
 
       const data = await res.json();
